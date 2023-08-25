74e01b846d329ef013a60eadad8ce0941783a21a
久米 拓馬 <takuma.kume@pepabo.com>
2023-08-25 21:47:07 +0900

    fix docs

--- cmd/root.go
+++ cmd/root.go
@@ -17,39 +17,48 @@ var rootCmd = &cobra.Command{
 	Short: "command line tool to receive JSON of SBOM Report from stdin",
 	Long: `Send Aqua Security Trivy Operator's SBOM Report to OWASP Dependency-Track.
 
+two ways to use:
+
 1. command line tool to receive JSON of SBOM Report from stdin
 
-	$ kubectl get sbom hoge -o json | sbomreport-to-dependencytrac
+  $ kubectl get sbom hoge -o json | sbomreport-to-dependencytrac
 
 2. http server that receives JSON of SBOM Report from Trivy Operator webhook
 
-	$ sbomreport-to-dependencytrack server --port 80
+  $ sbomreport-to-dependencytrack server --port 80
 
-Templates with the SBOM Report as a variable can be used for the following items to be registered in the Dependency Track.
+Templates with the SBOM Report as a variable can be used for the following items to be registered in the Dependency-Track.
 
-* Project Name
-* Project Version
-* Project Tags
+  * Project Name
+  * Project Version
+  * Project Tags
+  
+  $ kubectl get sbom hoge -o json | sbomreport-to-dependencytrack \
+      --base-url http://127.0.0.1:8081/ \
+      --api-key 1234567890 \
+      --project-name "[[.sbomReport.report.artifact.repository]]" \                  # e.g. "library/alpine"
+      --project-version "[[.sbomReport.report.artifact.tag]]" \                      # e.g. "3.13.5"
+      --project-tags "tag1,kube_namespace:[[.sbomReport.metadata.namespace]]" # e.g. ["tag1", "kube_namespace:default"]
 
-	$ kubectl get sbom hoge -o json | sbomreport-to-dependencytrack \
-	    --base-url http://127.0.0.1:8081/ \
-		--api-key 1234567890 \
-	    --project-name "[[.sbomReport.report.artifact.repository]]" \                  # e.g. "library/alpine"
-	    --project-version "[[.sbomReport.report.artifact.tag]]" \                      # e.g. "3.13.5"
-	    --project-tags "tag1,kube_namespace:[[.sbomReport.report.metadata.namespace]]" # e.g. ["tag1", "kube_namespace:default"]
-
-	For template, go-template and sprig functions can be used.
-	The delimiter of template is "[[" "]]". This is to avoid conflicts with other tools such as Helm.
+  For template, go-template and sprig functions can be used.
+  The delimiter of template is "[[" "]]". This is to avoid conflicts with other tools such as Helm.
 
 Environment variables can be used instead of command line arguments, which may be useful when running on Kubernetes.
 
-	$ kubectl get sbom hoge -o json | \
-		DT_BASE_URL=http://127.0.0.1:8081 \
-		DT_API_KEY=1234567890 \
-		DT_PROJECT_NAME="[[.sbomReport.report.artifact.repository]]" \
-		DT_PROJECT_VERSION="[[.sbomReport.report.artifact.tag]]" \
-		DT_PROJECT_TAGS="tag1,kube_namespace:[[.sbomReport.report.metadata.namespace]]" \
-		sbomreport-to-dependencytrack
+  $ kubectl get sbom hoge -o json | \
+      DT_BASE_URL=http://127.0.0.1:8081 \
+      DT_API_KEY=1234567890 \
+      DT_PROJECT_NAME="[[.sbomReport.report.artifact.repository]]" \
+      DT_PROJECT_VERSION="[[.sbomReport.report.artifact.tag]]" \
+      DT_PROJECT_TAGS="tag1,kube_namespace:[[.sbomReport.metadata.namespace]]" \
+      sbomreport-to-dependencytrack
+
+Dependency-Track APK key permissions required:
+
+  * BOM_UPLOAD 
+  * PORTFOLIO_MANAGEMENT 
+  * PROJECT_CREATION_UPLOAD 
+  * VIEW_PORTFOLIO 
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.Background()
